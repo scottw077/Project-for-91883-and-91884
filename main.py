a6d85b805d108d9aca80aa505d69d516992f281a
scottw077 <scottw@stu.otc.school.nz>
2022-08-09 19:09:36 -0230

    developing the magic 8 ball

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
 import time
+#creating the colour
+def colour(r, g, b, text):
+    return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r,g,b,text)
 
 #main menu
 def main_menu():
     while True:
-        menu = int(input("Press '0' to end program\nPress '1' to go to Magic 8 Ball\nPress '2' to go to Random Item from a Predefined List\nPress '3' to open Admin login menu\n"))
+        print(colour(106, 100, 230, "Type in the number corresponding to the Program you want to run"))
+        menu = int(input("Press '0' to end program\nPress '1' to go to Magic 8 Ball\nPress '2' to go to Random Item from a Predefined List\nPress '3' to open Admin login menu\n")) #asks the user what program they want to run
         if menu == 0:
             quit()
         elif menu == 1:
@@ -23,10 +27,13 @@ def main_menu():
 
 
 def magic_8_ball():
-    responses = ["My sources say no", "My sources say yes"]
-    response = responses[random.randint(1, len(responses))]
+    responses = [colour(210, 4, 35, "My sources say no"), colour(0, 255, 0, "My sources say yes"), colour(255, 140, 0, "I don't know"),
+                 colour(255, 140, 0, "Please re-enter question"), "Yeah obviously", "Clearly not", "Yeah, I am {} percent sure".format((random.randint(1, 100))),
+                 "No, I am {} percent sure".format((random.randint(1, 100)))]
     while True:
-        question = input("What would you like to ask the Magic 8 Ball? Yes/no questions only\nPress '0' to return to main menu\n")
+        response = responses[random.randint(1, len(responses))]
+        print(colour(106, 100, 230, "What would you like to ask the Magic 8 Ball? Yes/no questions only"))
+        question = input(colour(210, 4, 35, "Press '0' to return to main menu\n"))
         if question == "0":
             main_menu()
             break
