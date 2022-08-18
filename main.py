e5307094c9d8f4e6bf67e7aaadee1a0ee6f554a3
william <scottw@stu.otc.school.nz>
2022-08-18 21:25:28 +1200

    updated some stuff

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def colour(r, g, b, text):
     return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r,g,b,text)
 
 
+#the responses for the magic 8 ball
 responses = [colour(210, 4, 35, "My sources say no"), colour(0, 255, 0, "My sources say yes"), colour(255, 140, 0, "I don't know"),
              colour(255, 140, 0, "Please re-enter question"), colour(0, 255, 0, "Yeah obviously"), colour(210, 4, 35, "Clearly not"),
              colour(0, 255, 0, "Yeah, I am {} percent sure").format((random.randint(1, 100))), colour(210, 4, 35, "No, I am {} percent sure").format((random.randint(1, 100)))]
@@ -32,18 +33,19 @@ def main_menu():
 
 def magic_8_ball():
     while True:
-        response = responses[random.randint(0, len(responses) -1)]
-        print(colour(106, 100, 230, "\nWhat would you like to ask the Magic 8 Ball? Yes/no questions only"))
-        question = input(colour(255, 49, 49, "Press '0' to return to main menu\n"))
+        response1 = responses[random.randint(0, len(responses) -1)] #chooses a random response
+        print("\nWhat would you like to ask the Magic 8 Ball? Yes/no questions only")
+        question = input(colour(255, 49, 49, "Press '0' to return to main menu\n")) #asks the user the question they would like to ask magic 8 ball
         if question == "0":
             main_menu()
-            break
+            break #returns to menu
         else:
-            print("\nYour question: {}\nMagic 8 Ball's answer: {}".format(colour(0, 193, 255, question), response))
+            print("\nYour question: {}\nMagic 8 Ball's answer: {}".format(colour(0, 193, 255, question), response1)) #prints response
             time.sleep(1.4)
 
 
 def predefined_lists():
+    #the predefined lists
     fast_food = ["Subway", "Mcdonald's", "Burger King", "KFC", "Wendy's", "Domino's Pizza", "Pizza Hut", "Carl's Jr"]
     streaming_platforms = ["Netflix", "Disney+", "Neon", "Youtube", "Twitch", "Prime Video", "Hulu", "TVNZ", "Three Now"]
     burger_type = ["Cheeseburger", "Deluxe Burger", "Chicken Burger", "Steak Burger"]
@@ -54,8 +56,9 @@ def predefined_lists():
     while True:
         print(colour(106, 100, 230, "\nType in the number corresponding to the List you want a random item from"))
         the_predefined_lists = ("Press '0' to return to main menu\nPress '1' for Fast Food Restaurants\nPress '2' for Streaming Platforms\n"
-                                "Press '3' for Burger Type\nPress '4' for Pizza Type\nPress '5' for a Video game\n")
+                                "Press '3' for Burger Type\nPress '4' for Pizza Type\nPress '5' for a Video game\n") #asks the user what list they would like to use
         predefined_lists_input = int(input(the_predefined_lists))
+        #prints out a random item from the list they selected
         if predefined_lists_input == 0:
             main_menu()
             break
@@ -81,29 +84,30 @@ def predefined_lists():
 
 def admin_login_menu():
     while True:
-        admin_login_menu_input = input("Please input a username\nPress '0' to return to main menu\n").strip().lower()
-        if admin_login_menu_input == "0":
+        admin_login_menu_input = input("Please input a username\nPress '0' to return to main menu\n").strip().lower() #username input
+        if admin_login_menu_input == "0": #menu return
             main_menu()
             break
         elif admin_login_menu_input == "scottw":
-            admin_pass = input("Please input a password\nPress '0' to return to main menu\n")
-            if admin_pass == "0":
+            admin_pass = input("Please input a password\nPress '0' to return to main menu\n") #password input if username is correct
+            if admin_pass == "0": #return to main menu
                 main_menu()
                 break
-            elif admin_pass == "gruiscool123":
+            elif admin_pass == "gruiscool123": #if password is correct takes user to admin menu
                 print(colour(0, 255, 0, "Successfully signed in"))
                 admin_menu()
                 break
             else:
-                print(colour(210, 4, 35, "Wrong password please try again"))
+                print(colour(210, 4, 35, "Wrong password please try again")) #tells the user the password is incorrect
                 time.sleep(1)
         else:
-            print(colour(210, 4, 35, "Wrong username please try again"))
+            print(colour(210, 4, 35, "Wrong username please try again")) #tells the user the username is incorrect
             time.sleep(1)
 
 
 def admin_menu():
     while True:
+        print(colour(255, 195, 30, "\nWelcome to The Admin Menu"))
         admin_menu_selector = int(input("Press '0' to return to main menu\nPress '1' to open Magic 8 Ball backend\nPress '2' to open Predefined lists backend\n"))
         if admin_menu_selector == 0:
             main_menu()
@@ -119,7 +123,8 @@ def admin_menu():
 
 def magic_8_ball_backend():
     while True:
-        magic8_admin_selector = int(input("Press '0' to return to admin menu\nPress '1' to add responses\nPress '2' to remove responses\n"))
+        print(colour(255, 195, 30, "Magic 8 Ball Backend"))
+        magic8_admin_selector = int(input("Press '0' to return to admin menu\nPress '1' to add response to the magic 8 ball\nPress '2' to remove responses from the magic 8 ball\n"))
         if magic8_admin_selector == 0:
             admin_menu()
             break
@@ -138,16 +143,15 @@ def magic_8_ball_backend():
                 print(new_response)
                 while True:
                     confirmation = input("Are you sure you want to add this?\nPress 'y' for yes\nPress 'n' for no\n").strip().lower()
-                    print(colour(210, 4, 35, "Response successfully removed"))
                     if confirmation == "n":
+                        print(colour(210, 4, 35, "Response has not been added"))
                         magic_8_ball_backend()
                         new_response = ""
                         break
                         break
                     elif confirmation == "y":
                         responses.append(new_response)
-                        print(new_response)
-                        print(colour(0, 255, 0, "Successfully added to responses"))
+                        print("'{}' has been successfully added to responses".format(new_response))
                         break
                     else:
                         print(colour(210, 4, 35, "Invalid response please try again"))
@@ -159,15 +163,14 @@ def magic_8_ball_backend():
                 while True:
                     confirmation = input("Are you sure you want to add this?\nPress 'y' for yes\nPress 'n' for no\n").strip().lower()
                     if confirmation == "n":
-                        print(colour(210, 4, 35, "Response successfully removed"))
+                        print(colour(210, 4, 35, "Response has not been added"))
                         magic_8_ball_backend()
                         new_response = ""
                         break
                         break
                     elif confirmation == "y":
                         responses.append(new_response)
-                        print(new_response)
-                        print(colour(0, 255, 0, "Successfully added to responses"))
+                        print("'{}' has been successfully added to responses".format(new_response))
                         break
                     else:
                         print(colour(210, 4, 35, "Invalid response please try again"))
@@ -178,15 +181,14 @@ def magic_8_ball_backend():
                 while True:
                     confirmation = input("Are you sure you want to add this?\nPress 'y' for yes\nPress 'n' for no\n").strip().lower()
                     if confirmation == "n":
-                        print(colour(210, 4, 35, "Response successfully removed"))
+                        print(colour(210, 4, 35, "Response has not been added"))
                         magic_8_ball_backend()
                         new_response = ""
                         break
                         break
                     elif confirmation == "y":
                         responses.append(new_response)
-                        print(new_response)
-                        print(colour(0, 255, 0, "Successfully added to responses"))
+                        print("'{}' has been successfully added to responses".format(new_response))
                         break
                     else:
                         print(colour(210, 4, 35, "Invalid response please try again"))
@@ -194,19 +196,20 @@ def magic_8_ball_backend():
                 print(colour(210, 4, 35, "Invalid option please try again"))
 
         elif magic8_admin_selector == 2:
-                response in responses:
-                for i in range(1, 2):
-                    print("{}. {}".format(i, response))
+            num = 0
+            for response in responses:
+                num += 1
+                print("{}. {}".format(num, response))
+            response_del = int(input("What response would you like to delete? Please enter the number next to the response\n"))
+            del responses[(response_del-1)]
+            print(colour(210, 4, 35, "Response successfully removed"))
+        else:
+            print(colour(210, 4, 35, "Invalid input"))
+            time.sleep(0.8)
+
+def predefined_lists_backend():
+    while True:
+        print("pie")
 
 
-
-            response_del = input("What response would you like to delete? Please enter the number next to the response\n")
-            del responses[int(response_del)]
-            print(responses)
-
-
-
-
-
-
-magic_8_ball_backend()
+main_menu()
