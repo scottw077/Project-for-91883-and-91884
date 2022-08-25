81e5ebd9810e156283513919cea298ba6a8d2611
scottw077 <scottw@stu.otc.school.nz>
2022-08-26 10:59:05 +1200

    asdasdasd

--- main.py
+++ main.py
@@ -69,29 +69,11 @@ def predefined_lists():
             main_menu()
             break
         else:
-            with open('predefined_lists.json') as dict:
-                predefined_list = json.load(dict)
-                print(predefined_list[predefined_lists_input - 1])
-                print(predefined_list)
+            dict = open('predefined_lists.json')
+            predefined_list = json.load(dict)
+            j = predefined_list[predefined_lists_input-1]
+            print(j[random.randint(1, len(j))])
 
-elif predefined_lists_input == 1:
-   print(fast_food[random.randint(0, len(fast_food)-1)])
-   time.sleep(0.7)
-elif predefined_lists_input == 2:
-   print(streaming_platforms[random.randint(0, len(streaming_platforms)-1)])
-   time.sleep(0.7)
-elif predefined_lists_input == 3:
-   print(burger_type[random.randint(0, len(burger_type)-1)])
-   time.sleep(0.7)
-elif predefined_lists_input == 4:
-   print(pizza_type[random.randint(0, len(pizza_type)-1)])
-   time.sleep(0.7)
-elif predefined_lists_input == 5:
-   print(video_games[random.randint(0, len(video_games)-1)])
-   time.sleep(0.7)
-else:
-   print(colour(210, 4, 35, "That's not a valid option!"))
-   time.sleep(0.8)
 
 
 
--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-def pycn
-font_20 = pygame.font.Font(None, 20)
-text1 = font_20.render("Hello", ...)
+elif predefined_lists_input == 1:
+   print(fast_food[random.randint(0, len(fast_food)-1)])
+   time.sleep(0.7)
+elif predefined_lists_input == 2:
+   print(streaming_platforms[random.randint(0, len(streaming_platforms)-1)])
+   time.sleep(0.7)
+elif predefined_lists_input == 3:
+   print(burger_type[random.randint(0, len(burger_type)-1)])
+   time.sleep(0.7)
+elif predefined_lists_input == 4:
+   print(pizza_type[random.randint(0, len(pizza_type)-1)])
+   time.sleep(0.7)
+elif predefined_lists_input == 5:
+   print(video_games[random.randint(0, len(video_games)-1)])
+   time.sleep(0.7)
+else:
+   print(colour(210, 4, 35, "That's not a valid option!"))
+   time.sleep(0.8)
